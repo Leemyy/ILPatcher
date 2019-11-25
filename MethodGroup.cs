6fb9a7c6df0172114a133e032d8299bd9405888f
Leemyy <yunomanics@live.de>
2019-11-25 22:58:55 +0100

    Implement MethodGroup

--- MethodGroup.cs
+++ MethodGroup.cs
@@ -13,6 +13,9 @@ namespace ILPatcher
 		private int _count;
 		private int[] _hashes;
 		private T[] _overloads;
+		private int _optionalCount;
+		private int[] _optionalHashes;
+		private T[] _optionalOverloads;
 
 		public string Name { get; }
 		public int Count => _count;
@@ -21,32 +24,105 @@ namespace ILPatcher
 		{
 			_hashes = NoHashes;
 			_overloads = NoOverloads;
+			_optionalHashes = NoHashes;
+			_optionalOverloads = NoOverloads;
 		}
 
 
 		public void AddOverload(T method)
 		{
-			int slots = method.OptionalParameterCount + 1;
 			if (_count == _overloads.Length)
-				GrowBackingArrays(_count + slots);
+				GrowBackingArrays(_count + 1);
 
-			var parameters = method.Parameters;
-			int hash = method.GenericParameterCount;
-			var nonOptional = parameters.Count - method.OptionalParameterCount;
-			for (int i = 0; i < nonOptional; i++)
+			_overloads[_count] = method;
+			_hashes[_count] = HashOf(method, method.ParameterCount);
+
+			_count++;
+
+			AddOptionalOverloads(method);
+		}
+
+		private void AddOptionalOverloads(T method)
+		{
+			var count = method.OptionalParameterCount;
+			if (count == 0)
+				return;
+			if (_optionalCount + count > _overloads.Length)
+				GrowOptionalArrays(_optionalCount + count);
+
+			var nonOptional = method.ParameterCount - count;
+			for (int i = 0; i < count; i++)
 			{
-				hash ^= parameters[i].Type.GetHashCode();
+				_optionalHashes[_optionalCount + i] = HashOf(method, nonOptional + i);
+				_optionalOverloads[_optionalCount + i] = method;
+			}
+			_optionalCount += count;
+		}
+
+		public bool ConflictsWith(IMethod method)
+		{
+			var count = method.ParameterCount;
+			var nonOptional = count - method.OptionalParameterCount;
+			for (int i = nonOptional; i <= count; i++)
+			{
+				if (ConflictsWith(method, HashOf(method, i), i))
+					return true;
+			}
+			return false;
+		}
+
+		private bool ConflictsWith(IMethod method, int hash, int parameterCount)
+		{
+			for (int i = 0; i < _optionalHashes.Length; i++)
+			{
+				if (_optionalHashes[i] != hash)
+					continue;
+				if (!SignaturesMatch(_optionalOverloads[i], method))
+					continue;
+				return true;
+			}
+			for (int i = 0; i < _hashes.Length; i++)
+			{
+				if (_hashes[i] != hash)
+					continue;
+				if (!SignaturesMatch(_overloads[i], method))
+					continue;
+				return true;
+			}
+			return false;
+		}
+
+		public T FindMatch(IMethod method)
+		{
+			var hash = HashOf(method, method.ParameterCount);
+			T match = default;
+			bool exact = true;
+			for (int i = 0; i < _hashes.Length; i++)
+			{
+				if (_hashes[i] != hash)
+					continue;
+				if (!SignaturesMatch(_overloads[i], method))
+					continue;
+
+				if (match == default)
+					match = _overloads[i];
+				else
+				{
+					if (exact)
+					{
+						exact = false;
+						Console.WriteLine($"Ambiguous match, {method} matches more than one method:");
+						Console.Write("\t- ");
+						Console.WriteLine(match.ToString());
+					}
+					Console.Write("\t- ");
+					Console.WriteLine(_overloads[i].ToString());
+				}
 			}
 
-			for (int i = 0; i < slots; i++)
-			{
-				if (i > 0)
-					hash ^= parameters[nonOptional + i - 1].Type.GetHashCode();
-
-				_overloads[_count + i] = method;
-				_hashes[_count + i] = hash;
-			}
-			_count += slots;
+			if (exact)
+				return match;
+			return default;
 		}
 
 		private void GrowBackingArrays(int min)
@@ -60,6 +136,53 @@ namespace ILPatcher
 			Array.Copy(_overloads, growOverloads, _count);
 			_hashes = growHashes;
 			_overloads = growOverloads;
+		}
+
+		private void GrowOptionalArrays(int min)
+		{
+			var newSize = _optionalCount * 2;
+			if (newSize < min)
+				newSize = min;
+			var growHashes = new int[newSize];
+			var growOverloads = new T[newSize];
+			Array.Copy(_optionalHashes, growHashes, _optionalCount);
+			Array.Copy(_optionalOverloads, growOverloads, _optionalCount);
+			_optionalHashes = growHashes;
+			_optionalOverloads = growOverloads;
+		}
+
+
+		private static int HashOf(IMethod method, int parameterCount)
+		{
+			int hash = method.GenericParameterCount;
+			if (parameterCount == 0)
+				return hash;
+			var parameters = method.Parameters;
+			for (int i = 0; i < parameterCount; i++)
+			{
+				hash ^= parameters[i].Type.GetHashCode();
+			}
+			return hash;
+		}
+
+		private static bool SignaturesMatch(T left, IMethod right)
+		{
+			if (left.GenericParameterCount != right.GenericParameterCount)
+				return false;
+			if (left.ParameterCount != right.ParameterCount)
+				return false;
+
+			var leftParams = left.Parameters;
+			var rightParams = right.Parameters;
+			var count = leftParams.Count;
+			for (int i = 0; i < count; i++)
+			{
+				if (leftParams[i].Policy.IsByRef() != rightParams[i].Policy.IsByRef() ||
+					leftParams[i].Type != rightParams[i].Type)
+					return false;
+			}
+
+			return true;
 		}
 	}
 }
--- Model/ParameterPolicy.cs
+++ Model/ParameterPolicy.cs
@@ -14,4 +14,22 @@ namespace ILPatcher.Model
 		Out,
 		Params
 	}
+
+	public static class ParameterPolicyHelper
+	{
+		public static bool IsByVal(this ParameterPolicy policy)
+		{
+			return
+				policy == ParameterPolicy.Value ||
+				policy == ParameterPolicy.Params;
+		}
+
+		public static bool IsByRef(this ParameterPolicy policy)
+		{
+			return
+				policy == ParameterPolicy.Reference ||
+				policy == ParameterPolicy.In ||
+				policy == ParameterPolicy.Out;
+		}
+	}
 }
